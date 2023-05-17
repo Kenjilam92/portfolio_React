4a81005e9c0789ced1333666579b54f91e594d5c
Trong Kien Ho <62187768+Kenjilam92@users.noreply.github.com>
2023-05-16 23:35:33 -0400

    Update ResumeAPI.js
    added new work experiences

--- src/APIs/ResumeAPI.js
+++ src/APIs/ResumeAPI.js
@@ -1,27 +1,29 @@
 const ResumeAPI =[
     {
-        period: "2010-2014",
-        type: "education",
-        title: "Bacherlor of Marketing Administration",
-        organization: "Open University",
-        description: "",
-        url: "http://en.ou.edu.vn/",
-        keywords:[],
-        bulletPoints: [
+        "period": "2010-2014",
+        "type": "education",
+        "title": "Bacherlor of Marketing Administration",
+        "organization": "Open University",
+        "description": "",
+        "url": "http://en.ou.edu.vn/",
+        "keywords":[],
+        "bulletPoints": [
             "Achieved a Bachelor’s in Marketing Administration, GPA: 3.56",
-            "Demonstrated marketing strategies by collecting and analyzing the data from the market.",
-            "Had being class president from 2011 to 2012"
+            "Demonstrated marketing strategies based on real data",
+            "Collected market survey and converted into database",
+            "Analyzed survey results from a sample size of 100 and defined customer behaviors, also market outlook",
+            "Honorably became class president from 2011 to 2012"
         ]
     },
     {
-        period: "2014",
-        type: "education",
-        title: "SEO specialist",
-        organization: "Nhat Nghe Academy (Microsoft Gold Certified Partner)",
-        description: "",
-        url: "https://nhatnghe.com/",
-        keywords:[],
-        bulletPoints: [
+        "period": "2014",
+        "type": "education",
+        "title": "SEO specialist",
+        "organization": "Nhat Nghe Academy (Microsoft Gold Certified Partner)",
+        "description": "",
+        "url": "https://nhatnghe.com/",
+        "keywords":[],
+        "bulletPoints": [
             "Archived Search Engine Optimization Specialist Certificate",
             "Improved the popularity rate of websites on Google Search.",
             "Accumulated 200+ hours in using Google Analytics, SEOQuake, and the other analyzing tools",
@@ -30,41 +32,57 @@ const ResumeAPI =[
     },
 
     {
-        period: "2014-2016",
-        type: "experience",
-        title: "Digital Marketer",
-        organization: "Van Hoa Co., Ltd",
-        description: "",
-        url: "https://www.vanhoagroup.com/",
-        keywords:["SEO"],
-        bulletPoints: [
-            "Achieved a Bachelor’s in Marketing Administration, GPA: 3.56",
-            "Demonstrated marketing strategies by collecting and analyzing the data from the market.",
-            "Had being class president from 2011 to 2012"
+        "period": "2014-2016",
+        "type": "experience",
+        "title": "Digital Marketer",
+        "organization": "Van Hoa Co., Ltd",
+        "description": "",
+        "url": "https://www.vanhoagroup.com/",
+        "keywords":["SEO"],
+        "bulletPoints": [
+            "Provided SEO strategies that focus on target customer",
+            "Successful promoted website on top 3 Google Search result with keywords 'moc khoa' which mean 'keychain' in Vietnamese",
+            "Designed UX/UI for vanhoagroup.com which increased user traffic",
+            "Applied automation responses for common questions on Facebook pages and web chatbots, helped sale team educate customers more efficiency and improved user experiences"
         ]
     },
     {
-        period: "2017-2019",
-        type: "education",
-        title: "Associate Business Administration ",
-        organization: "Northern Virginia Community College",
-        description: "",
-        url: "https://www.nvcc.edu/",
-        keywords:[],
-        bulletPoints: [
-            "Achieved Associate Science in Business Administration, GPA: 3.8",
-            "Studied American Culture, Business, and Microbiology."
+        "period": "2017-2019",
+        "type": "education",
+        "title": "Associate Business Administration ",
+        "organization": "Northern Virginia Community College",
+        "description": "",
+        "url": "https://www.nvcc.edu/",
+        "keywords":[],
+        "bulletPoints": [
+            "Graduated Associate Science in Business Administration, GPA: 3.8",
+            "Honorably became a member of the Nation Society of Collegiate Scholars",
+            "Archived high performance in courses of Art Appreciation, Business Analysis, Accounting, and Microbiology"
         ]
     },
     {
-        period: "2020",
-        type: "education",
-        title: "Black Belt Fullstack developer",
-        organization: "Coding Dojo",
-        description: "",
-        url: "https://www.codingdojo.com/",
-        keywords:["Black belt", "MERN", "C#", "Python", "Django", "React", "MongoDB", "ExpressJS", "JavaScript"],
-        bulletPoints: [
+        "period": "2018-2020",
+        "type": "experience",
+        "title": "Medical Interpreter",
+        "organization": "Liberty Language Services",
+        "description": "",
+        "url": "www.libertylanguageservices.com",
+        "keywords":[],
+        "bulletPoints": [
+            "Provided steady interpreting conversations in medical fields",
+            "Helped ESL patients communicate with medical teams",
+            "Mentally supported patients and family members during difficult times"
+        ]
+    },
+    {
+        "period": "2020",
+        "type": "education",
+        "title": "Black Belt Fullstack developer",
+        "organization": "Coding Dojo",
+        "description": "",
+        "url": "https://www.codingdojo.com/",
+        "keywords":["Black belt", "MERN", "C#", "Python", "Django", "React", "MongoDB", "ExpressJS", "JavaScript"],
+        "bulletPoints": [
             "Achieved Full-stack Developer Certificate with the highest grade (3 black belts)",
             "Invested +1000 hours in developing coding in difference frameworks (Django, Flask, MERN, ASP.NET Core MVC)",
             "Completed projects independently or cooperatively in different projects (hackathon events, graduation projects, etc.)",
@@ -73,34 +91,52 @@ const ResumeAPI =[
         ]
     },
     {
-        period: "2020-2021",
-        type: "experience",
-        title: "Intern Website Developer",
-        organization: "AllPeople",
-        description: "",
-        url: "https://allpeoplemarket.com/",
-        keywords:[],
-        bulletPoints: [
+        "period": "2020-2021",
+        "type": "experience",
+        "title": "Intern Website Developer",
+        "organization": "AllPeople",
+        "description": "",
+        "url": "https://allpeoplemarket.com/",
+        "keywords":[],
+        "bulletPoints": [
             "A start-up company which's main goal is fundraising for non-profit organizations.",
             "Applied SCRUM methodology to plan a new project.",
             "Constructed API response for the company website which is using the Shopify framework."
         ]
     },
     {
-        period: "2021-current",
-        type: "expericent",
-        title: "Software Developer",
-        organization: "Revature",
-        description: "",
-        url: "https://revature.com/",
-        keywords:["Java", "Angular", "Spring", "Hibernate", "Oracal", "DevOps", "Micro Services"],
-        bulletPoints: [
-            "Completed 10 weeks training of Java, Spring, Angular, Microservices",
-            "Mantained fullstack projected from different team",
-            "Created new features, Angular components, and refactored data structure",
-            "Used Angile Methodology to manage project"
+        "period": "2021-2022",
+        "type": "experience",
+        "title": "Production Support",
+        "organization": "CVS",
+        "description": "",
+        "url": "www.cvs.com",
+        "keywords":["Java", "Angular", "Spring", "Hibernate", "Oracle", "DevOps", "Micro Services", "ServicesNOW"],
+        "bulletPoints": [
+            "Maintained a full-stack project that helped medical team fastening documentation process which follows HIPAA standard ",
+            "Managed code base on Git repositories, merged code across branches",
+            "Performed deployment through Jenkins pipelines which follow CI/CD standard ",
+            "Analyzed database, identified incorrect records, and performed massive correction",
+            "Distributed tickets and generated reports on Service NOW"
+        ]
+    },
+    {
+        "period": "2022-2023",
+        "type": "experience",
+        "title": "Software Developer",
+        "organization": "Freddie Mac",
+        "description": "",
+        "url": "www.freddiemac.com",
+        "keywords":["Java", "Angular", "Spring", "Hibernate", "Oracle", "DevOps", "Micro Services", "ServicesNOW"],
+        "bulletPoints": [
+            "Developed an internal full stack project that helped teams access database and generated reports with friendly UI",
+            "Designed reusable Angular for financial report templates which fastened development process",
+            "Applied Agile methodology in managing the project",
+            "Upgraded technologies for other existing external projects on a high business level",
+            "Provided and coordinated implementation plans and backout plans for deployments"    
         ]
     }
 ]
+
 
 export default ResumeAPI;
