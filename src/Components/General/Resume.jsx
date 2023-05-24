1f22dc6d91417f8d34a7eb89f95a96732388f096
Kenji Lam <kenji.lam.92@gmail.com>
2023-05-23 21:56:00 -0400

    Add responsive feature for resume component

--- src/Components/General/Resume.jsx
+++ src/Components/General/Resume.jsx
@@ -21,33 +21,51 @@ const Resume = props => {
 
             {/* <script src="https://gist.github.com/Kenjilam92/8b977036b5f33ba4a559c01e5815528f.js"></script> */}
 
-            {
-                ResumeAPI.map((ex,k) => 
-                    <div key={"Resume"+k} className="text-white row">
-                        <div className={`col-3 w-fit text-right text-warning`}>
-                        {/* text-${ex.type==="education"?"info":"warning"} */}
-                            <b>{ex.period}</b>
-                        </div>
-                        <p>
-                            &#x25cf;
-                        </p>
-                        <div className="col">
-                            <b >
-                                <a href={ex.url} className={`text-warning`}>
-                                    {ex.organization} 
-                                </a>
-                            </b>
-                            <ul>
-                                {ex.bulletPoints.map((bP,key) => 
-                                    <li key={ex.period+"bP"+key}>
-                                        {bP}  
-                                    </li>
-                                )}
-                            </ul>
-                        </div>
-                    </div>
-                )
-            }
+            {ResumeAPI.map((ex,k) => 
+                <div key={"Resume"+k} className="text-white row laptop">
+                  <div className={`col-3 w-fit text-right text-warning`}>
+                  {/* text-${ex.type==="education"?"info":"warning"} */}
+                      <b>{ex.period}</b>
+                  </div>
+                  <p>
+                      &#x25cf;
+                  </p>
+                  <div className="col">
+                      <b >
+                          <a href={ex.url} className={`text-warning`}>
+                              {ex.organization} 
+                          </a>
+                      </b>
+                      <ul>
+                          {ex.bulletPoints.map((bP,key) => 
+                              <li key={ex.period+"bP"+key}>
+                                  {bP}  
+                              </li>
+                          )}
+                      </ul>
+                  </div>
+                </div>
+            )}
+            {ResumeAPI.map((ex,k) => 
+                <div key={"Resume"+k} className="text-white row phone">
+                  <div className={`col w-fit`}>
+                  {/* text-${ex.type==="education"?"info":"warning"} */}
+                      <p> &#x25cf;
+                        <b className="text-warning">{ex.period}</b>
+                      </p>
+                      <div className="col">
+                          <b><a href={ex.url} className={`text-warning`}>
+                            {ex.organization} 
+                          </a></b>
+                          <ul>{ex.bulletPoints.map((bP,key) => 
+                                  <li key={ex.period+"bP"+key}>
+                                      {bP}  
+                                  </li>
+                          )}</ul>
+                      </div>
+                  </div>
+                </div>
+            )}
         </div>
     );
 }
